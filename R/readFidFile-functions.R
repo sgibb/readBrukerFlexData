1628dea7ceb0646dea31a27f96f754c747a8216a
Sebastian Gibb <mail@sebastiangibb.de>
2013-05-12 09:54:12 +0200

    always return intensity values as double

--- R/readFidFile-functions.R
+++ R/readFidFile-functions.R
@@ -24,12 +24,12 @@
 #'
 #' @param fidFile \code{character} path to fid file e.g.
 #'  Pankreas_HB_L_061019_A10/0_a19/1/1SLin/fid
-#' @param nIntensities number of data entries 
-#'  (total count; get from acqu file) 
+#' @param nIntensities number of data entries
+#'  (total count; get from acqu file)
 #' @param endian endianness of the fid file
 #'  (\sQuote{little} or \sQuote{big}; default: \sQuote{little})
-#' 
-#' @return 
+#'
+#' @return
 #'  A vector of intensity values.
 #' @seealso
 #'  \code{\link[readBrukerFlexData]{.readAcquFile}},
@@ -41,11 +41,11 @@
   if (!file.exists(fidFile)) {
     stop("File ", sQuote(fidFile), " doesn't exists!")
   }
-  
+
   con <- file(fidFile, "rb")
   intensity <- readBin(con, integer(), n=nIntensities, size=4, endian=endian)
   close(con)
-  
-  return(intensity)
+
+  return(as.double(intensity))
 }
- 
+
