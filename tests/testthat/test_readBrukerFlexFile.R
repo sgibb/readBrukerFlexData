e2c7b83447166dec4bb2265a126f9012fb5ee462
Sebastian Gibb <mail@sebastiangibb.de>
2025-10-17 22:19:38 +0200

    chore: move to codeberg.org

--- tests/testthat/test_readBrukerFlexFile.R
+++
@@ -1,5 +0,0 @@
-test_that("general", {
-    expect_error(readBrukerFlexFile("./"))
-    expect_error(readBrukerFlexFile("test_readBrukerFlexFile.R"))
-    expect_error(readBrukerFlexFile("ape"))
-})
